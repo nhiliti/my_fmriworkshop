c4470d09d44d01a6f5ecf9ba3d5be389f5a690d6
nhiliti <nhidinh.l@gmail.com>
2017-12-12 11:15:55 -0500

    update the prepo.py script

--- prepro.py
+++ prepro.py
@@ -5,4 +5,16 @@ Created on Tue Dec 12 10:54:00 2017
 
 @author: cnglab
 """
+import glob
+import os
+import shutil
 
+def prepo(basedir):
+    print('Hello data in the path '+basedir)
+    
+def main():
+    #load in all the global variables prepro needs
+    basedir='/Users/cnglab/Desktop/data'
+    prepo(basedir) #call prepo to do things 
+    
+main()
